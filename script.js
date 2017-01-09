2b8e3143811b6da2bd530945d52714c84ccb58df
Robert Guilbe <robertguilbeiii@students.ptechnyc.org>
2017-01-09 15:10:17 +0000

    smileycornerheart

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //Write your function calls below this line
+
+//corners (yelllow)
+setColor(0,0, "yellow");
+setColor(0,9, "yellow");
+setColor(9,0, "yellow");
+setColor(9,9, "yellow");
+//smiley (below) (red)
+setColor(2,2, "Red");
+setColor(2,7, "red");
+setColor(5,2, "red");
+setColor(6,3, "red");
+setColor(6,4, "red");
+setColor(6,5, "red");
+setColor(6,6, "red");
+setColor(5,7, "red");
+//heart (bellow) (black)
+setColor(2,4, "black");
+setColor(2,5, "black");
+setColor(1,3, "black");
+setColor(1,6, "black");
+setColor(1,2, "black");
+setColor(1,7, "black");
+setColor(2,8, "black");
+setColor(3,9, "black");
+setColor(2,1, "black");
+setColor(3,0, "black");
+setColor(4,0, "black");
+setColor(5,0, "black");
+setColor(4,9, "black");
+setColor(5,9, "black");
+setColor(6,1, "black");
+setColor(6,8, "black");
+setColor(7,2, "black");
+setColor(7,7, "black");
+setColor(8,3, "black");
+setColor(8,6, "black");
+setColor(9,4, "black");
+setColor(9,5, "black");
